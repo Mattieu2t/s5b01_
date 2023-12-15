fd67b45c53611ae9adf96583beddc1bb7937d311
Younes Ben Dhiab <younes.bendhiab.etu@univ-lille.fr>
2023-12-15 18:26:02 +0100

    ldap et nfs done

--- equipe-c/doc/ldap-nfs/bin/client_nfs_mount.sh
+++ equipe-c/doc/ldap-nfs/bin/client_nfs_mount.sh
@@ -4,7 +4,7 @@ NFS_IP=192.168.21.249
 
 apt install -y nfs-common
 mkdir /home/nfs_home
-mount -t nfs $NFS_IP:/home/nfs_home/ /home/nfs_home
+mount -t nfs $NFS_IP:/home/nfs_home /home/nfs_home
 echo "$NFS_IP:/home/nfs_home /home/nfs_home nfs defaults 0 0" >> /etc/fstab
 
-mount /home/nfs_home/
+mount /home/nfs_home
